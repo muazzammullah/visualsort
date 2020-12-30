dbe0f509bdb896dd0db2149f06688e57777b6481
Muazzam Mullah <zmz.muazzammullah@gmail.com>
2020-12-30 17:04:10 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ async function bubblesort(a){
     document.getElementById("key2").style.display="inline-block";   
     document.getElementById("key1").style.display="none";   
     var element_last_sorted=a.length-1;
+    sorted=[];
     for (i = 0; i<a.length; i++) {
         for ( j = 0 ; j < a.length-1-i; j++) {
-                draw(a);
                 document.getElementById('bar_'+j).style.backgroundColor = 'lightgrey';
                 document.getElementById('bar_'+(j+1)).style.backgroundColor = 'lightgrey';
                 if(a[j]>a[j+1]){
@@ -14,11 +14,11 @@ async function bubblesort(a){
                     a[j]=a[j+1];
                     a[j+1]=temp
                 }
-                await(sleep(80));
-                draw(a);
+                await(sleep(60));
+                draw(a,sorted);
         }
-        //element_last_sorted=a.length-1-i;
-        console.log(element_last_sorted);
+        element_last_sorted=a.length-1-i;
+        document.getElementById('bar_'+(element_last_sorted)).classList.add("bar_sorted");
     }
     alert("Sorting Complete");
     document.getElementById("startbutton").disabled=false;   
@@ -28,9 +28,13 @@ async function bubblesort(a){
 async function selsort(a){
     document.getElementById("key1").style.display="inline-block";
     document.getElementById("key2").style.display="none";     
+    var sorted=[];
     for(j=0;j<a.length;j++){
-        //sleep(200);
         //set min index to current j
+        try{
+        	document.getElementById('bar_'+(j)).classList.add("bar_sorted");
+        }
+        catch(err){}
         document.getElementById('bar_'+j).style.backgroundColor = 'grey';
         var currentmin=j;
         for(i=j+1;i<a.length;i++){
@@ -42,21 +46,18 @@ async function selsort(a){
                 document.getElementById('bar_'+currentmin).style.color = '#f1c40f';
                 for (x = j ; x <currentmin; x++) {
                   document.getElementById('bar_'+x).style.color = 'red';
-                }
-                
+                }  
             }
-            await(sleep(40));
+            await(sleep(60));
             document.getElementById('bar_'+i).style.backgroundColor = '#34495e';
         }
-        //keep current minimum as placeholder variable               
+        //keep current minimum as placeholder variable       
         place=a[j];
         //set new minimum in array in place of previous minimum
         a[j]=a[currentmin];
         //keep prev minimum in place of current/new minimum
         a[currentmin]=place;
-        await(sleep(40));
-        draw(a);
-        document.getElementById('bar_'+j).style.backgroundColor = '#34495e';
+        draw(a,sorted);
     }
     alert("Sorting Complete");
     document.getElementById("startbutton").disabled=false;   
@@ -66,11 +67,31 @@ function sleep(ms){
     return new Promise(resolve=>setTimeout(resolve,ms))
 }
 
-function draw(arr_to_sort){
+function draw(arr_to_sort,sorted){
+	for(y=0;y<arr_to_sort.length;y++){
+		try{
+		// console.log(document.getElementById("bar_"+y).classList.contains('bar_sorted'));
+		 if(document.getElementById("bar_"+y).classList.contains('bar_sorted')){
+		 	sorted.push(y);
+		 }
+		}
+		catch(err){
+			console.log("no bars yet");
+		}
+	}
     document.getElementById("root").innerHTML="";
     for(y=0;y<arr_to_sort.length;y++){
-        document.getElementById("root").innerHTML+="<div id='bar_"+y+"' class='bar' style='height:"+(4 *arr_to_sort[y])+"px;width:"+(100/arr_to_sort.length).toString()+"%'>"+arr_to_sort[y]+"</div>"
+			document.getElementById("root").innerHTML+="<div id='bar_"+y+"' class='bar' style='height:"+(4 *arr_to_sort[y])+"px;width:"+(100/arr_to_sort.length).toString()+"%'>"+arr_to_sort[y]+"</div>";
     }
+    console.log(sorted);
+    try{
+	 	for(y=0;y<sorted.length;y++){
+			document.getElementById('bar_'+(sorted[y])).style.backgroundColor = 'darkgreen';
+	    }
+	}
+	catch(err){
+		console.log('no bars');
+	}
 }
 
 function startvisualiser(num_elements){
@@ -88,7 +109,7 @@ function startvisualiser(num_elements){
     else if(document.getElementById("algo").value=="bubble"){
          bubblesort(arr_to_sort);
     }
-   }
+}
 
 
 //selsort([1,5,4,3,2]);
@@ -100,5 +121,4 @@ function start(){
         startvisualiser(document.getElementById("num_elements").value); 
         document.getElementById("startbutton").disabled=true;   
     }
-     
 }
